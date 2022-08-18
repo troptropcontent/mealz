da29f8f64c16e07ee1e72d1eae08bb91c6a83c29
Tom Ecrepont <tomecrepont@gmail.com>
2022-08-18 18:49:09 +0200

    set up devise to use username oinly

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, authentication_keys: [:username]
+  validates :username, uniqueness: true, presence: true
+
+  private
+  def email_required?
+    false
+  end
 end
