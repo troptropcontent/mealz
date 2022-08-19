efbdb672e9d42c8bdbd17ade374ca243fc980ba1
Tom Ecrepont <tomecrepont@gmail.com>
2022-08-19 15:43:20 +0200

    create ingredient

---
+++ db/migrate/20220819134222_create_ingredients.rb
@@ -0,0 +1,10 @@
+class CreateIngredients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :ingredients do |t|
+      t.string :name, null: false
+      t.integer :unit, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_19_125140) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_19_134222) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_19_125140) do
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_guests_on_event_id"
     t.index ["user_id"], name: "index_guests_on_user_id"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "unit", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "meals", force: :cascade do |t|
