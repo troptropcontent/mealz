2120f97376c06bbb1b736c65e7c79a9d7cc3bd52
Tom Ecrepont <tomecrepont@gmail.com>
2022-09-03 00:18:32 +0200

    update ui

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,47 +1,48 @@
 class CoursesController < ApplicationController
-  
   def new
-    @course = Course.new(course_params)
+    @course = Course.new
     @meal = Meal.find(params[:meal_id])
   end
 
   def create
     @course = Course.new(course_params)
     RecipeParamsHandler.new(@course, recipe_params).call
-    
+
     redirect_to meal_path(@course.meal)
   end
 
   def show
     load_course
-    
   end
 
   def update
     load_course
     RecipeParamsHandler.new(@course, recipe_params).call
-    
+
     redirect_to meal_path(@course.meal)
   end
 
   def destroy
-    @course.destroy
-    redirect_to meal_path(@course.meal)
+    course = Course.find(params[:id])
+    course.destroy
+    redirect_to meal_path(course.meal)
   end
- 
+
   private
 
   def course_params
     params.require(:course).permit(
-      :name, 
-      :description, 
-      :meal_id, 
-      :kind, 
-      )
+      :name,
+      :description,
+      :meal_id,
+      :kind
+    )
   end
 
   def recipe_params
-    params.require(:course).permit(recipes_attributes: [:quantity, ingredient: [:unit, :name]])[:recipes_attributes].values
+    params.require(:course).permit(recipes_attributes: [:quantity, {
+                                     ingredient: %i[unit name]
+                                   }])[:recipes_attributes].values
   end
 
   def load_course
