f6992b5b6292cd11b1818f743cd67c3329a56514
Tom Ecrepont <tomecrepont@gmail.com>
2022-09-02 11:55:41 +0200

    add validation on attribute presences of events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,13 +1,12 @@
 class Event < ApplicationRecord
-    has_many :guests, dependent: :destroy
-    has_many :meals, dependent: :destroy
-    has_many :courses, through: :meals
-    has_many :recipes, through: :courses
+  has_many :guests, dependent: :destroy
+  has_many :meals, dependent: :destroy
+  has_many :courses, through: :meals
+  has_many :recipes, through: :courses
 
+  validates :name, :start_on, :end_on, presence: true
 
-    def days
-        (start_on..end_on).to_a
-    end
+  def days
+    (start_on..end_on).to_a
+  end
 end
-
-
