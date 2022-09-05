7ed0b978917ea5c5687750cae4266fa60731acf9
Tom Ecrepont <tomecrepont@gmail.com>
2022-09-05 13:23:29 +0200

    add an association to event from course -through meal

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :meal
+  has_one :event, through: :meal
   has_many :recipes, dependent: :destroy
   has_many :ingredients, through: :recipes
   accepts_nested_attributes_for :recipes
