b7fa70c3b8250e881871e5260fc8e8a3bbdc9a8b
Tom Ecrepont <tomecrepont@gmail.com>
2022-09-09 11:38:12 +0200

    add collapsable behaviour

--- app/javascript/controllers/expandable_card_controller.js
+++ app/javascript/controllers/expandable_card_controller.js
@@ -1,14 +1,48 @@
 import { Controller } from "@hotwired/stimulus"
+import { useClickOutside } from 'stimulus-use'
 
 // Connects to data-controller="expandable-card"
 export default class extends Controller {
   static targets = [ "expandable", "chevronUp", "chevronDown" ]
   connect() {
+    useClickOutside(this)
+  }
+
+  get siblings() {
+    const isSibling = ({identifier, element}) => identifier == this.identifier && element != this.element
+    return this.application.controllers.filter(controller => isSibling(controller))
   }
 
   toggleExpandable() {
-    this.expandableTarget.classList.toggle("hidden")
-    this.chevronUpTarget.classList.toggle("hidden")
-    this.chevronDownTarget.classList.toggle("hidden")
+    const isCollapsed = this.expandableTarget.classList.contains('hidden')
+
+    if (isCollapsed) {
+      this.closeOtherCollapsables()
+      this.expand()
+    } else {
+      this.collapse()
+    }
+
+  }
+
+  closeOtherCollapsables(){
+    this.siblings.forEach(sibling => sibling.collapse())
+  }
+
+  collapse(){
+    this.expandableTarget.classList.add('hidden')
+    this.chevronUpTarget.classList.add('hidden')
+    this.chevronDownTarget.classList.remove('hidden')
+  }
+
+  expand(){
+    this.expandableTarget.classList.remove('hidden')
+    this.chevronDownTarget.classList.add('hidden')
+    this.chevronUpTarget.classList.remove('hidden')
+  }
+
+  clickOutside(event) {
+    event.preventDefault()
+    this.collapse()
   }
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'flatpickr', to: 'https://ga.jspm.io/npm:flatpickr@4.6.13/dist/esm/index.js'
 pin 'flatpickr/dist/l10n/fr', to: 'https://ga.jspm.io/npm:flatpickr@4.6.13/dist/l10n/fr.js'
+pin 'stimulus-use', to: 'https://ga.jspm.io/npm:stimulus-use@0.50.0/dist/index.js'
+pin 'hotkeys-js', to: 'https://ga.jspm.io/npm:hotkeys-js@3.10.0/dist/hotkeys.esm.js'
