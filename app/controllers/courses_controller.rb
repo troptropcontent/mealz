a7eec00f9fbaf58febfa97dfb317e4cd572b220d
Tom Ecrepont <tomecrepont@gmail.com>
2022-09-28 00:40:02 +0200

    create a proper update

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -16,6 +16,18 @@ class CoursesController < ApplicationController
     end
   end
 
+  def update
+    @meal = Meal.find_by(meal_params)
+    @event = @meal.event
+    @course = Course.find(params[:id])
+    
+    if @course.update(meal: @meal, **course_params)
+      redirect_to meal_path(@course.meal)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end  
+
   def show
     load_course
     @event = @course.event
